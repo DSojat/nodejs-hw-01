41157e2b793142785d6c523c9741694038fb4c52
DSojat <145054801+DSojat@users.noreply.github.com>
2024-06-09 21:41:02 +0300

    step 6

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+import fs from 'node:fs/promises';
+
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([]), 'utf8');
+    console.log('Дані успішно видалені з файлу.');
+  } catch (error) {
+    console.error('Помилка видалення даних з файлу:', error);
+  }
+};
 
 await removeAllContacts();
