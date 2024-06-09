59f1b31b2c383c8893874570e28ce63fbbde8a7b
DSojat <145054801+DSojat@users.noreply.github.com>
2024-06-09 21:57:57 +0300

    step 7

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-export const thanos = async () => {};
+import fs from 'node:fs/promises';
+
+export const thanos = async () => {
+  try {
+    const fileContent = JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
+
+    for (let index = 0; index < fileContent.length; index++) {
+      if (Math.random() >= 0.5) {
+        fileContent.pop();
+      }
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(fileContent), 'utf8');
+    console.log('Дані успішно змінено.');
+    console.log(fileContent.length);
+  } catch (error) {
+    console.error('Помилка додавання даних до файлу:', error);
+  }
+};
 
 await thanos();
