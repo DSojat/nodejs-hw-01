38e8be1abbcff31cbffdeb4499b55ed2e0de154e
DSojat <145054801+DSojat@users.noreply.github.com>
2024-06-09 21:20:58 +0300

    step 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,25 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+import fs from 'node:fs/promises';
+
+const generateContacts = async number => {
+  const data = [];
+  for (let index = 0; index < number; index++) {
+    data.push(createFakeContact());
+  }
+
+  try {
+    const fileContent = JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
+    fileContent.push(...data);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(fileContent), 'utf8');
+    console.log('Дані успішно записані у файл.');
+
+    // console.log(fileContent.length);
+  } catch (error) {
+    console.error('Помилка додавання даних до файлу:', error);
+  }
+};
 
 await generateContacts(5);
