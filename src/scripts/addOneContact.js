6cdc8a1ad04b7f62fb9dfbd5b96009f33b464ebc
DSojat <145054801+DSojat@users.noreply.github.com>
2024-06-09 21:24:45 +0300

    step 3

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+import fs from 'node:fs/promises';
+
+export const addOneContact = async () => {
+  const data = createFakeContact();
+
+  try {
+    const fileContent = JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
+    fileContent.push(data);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(fileContent), 'utf8');
+    console.log('Дані успішно записані у файл.');
+
+    // console.log(fileContent.length);
+  } catch (error) {
+    console.error('Помилка додавання даних до файлу:', error);
+  }
+};
 
 await addOneContact();
